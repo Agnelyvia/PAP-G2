3a597ed4ea7997a7213001da398995dabae7d5e5
Agnelyvia <96179966+Agnelyvia@users.noreply.github.com>
2023-01-31 10:22:33 +0000

    Add files via upload

--- RFID_Final_Lock/RFID_Final_Lock.ino
+++ RFID_Final_Lock/RFID_Final_Lock.ino
@@ -4,57 +4,47 @@
 #include <SPI.h>
 #include <LiquidCrystal.h>
 
-#define RST_PIN 9  // Configurable, see typical pin layout above
-#define SS_PIN 10  // Configurable, see typical pin layout above
-#define servoPin A2
-#define freeLED A5
-#define occupiedLED A4
-#define Buzzer A3
+#define RST_PIN 9          // define the RST pin to pin 10
+#define SS_PIN 10          // define the SS pin to pin 10
+#define servoPin A2        //define the Servo to pin A2
+#define freeLED A5         //define the Servo to pin A5
+#define occupiedLED A4     //define the Servo to pin A4
+#define Buzzer A3          //define the Servo to pin A3
+const int doorButton = 2;  //define the button pin to pin 2
 
-const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;  //define the LCD pins
 
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-const int doorButton = 2;
-
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);  //create an LCD instance to control
 
 
-bool lockState = true;
-bool verifyNameOfCard(MFRC522::MIFARE_Key key, byte block, byte len, MFRC522::StatusCode status);
 
-Servo lockServo;
+bool lockState = true;                                                                             //define the lockState to true, locked
+bool verifyNameOfCard(MFRC522::MIFARE_Key key, byte block, byte len, MFRC522::StatusCode status);  //function to verify the name of the card
 
-
+Servo lockServo;  //create a Servo instance
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
-void openDoor();
-void closeDoor();
-void doorNotClosed();
-void cardDenied();
-
 void setup() {
-  Serial.begin(9600);  // Initialize serial communications with the PC
-  SPI.begin();         // Init SPI bus
-  lcd.begin(16, 2);
-  mfrc522.PCD_Init();  // Init MFRC522 card
-  pinMode(servoPin, OUTPUT);
-  pinMode(freeLED, OUTPUT);
-  pinMode(occupiedLED, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
-  pinMode(doorButton, INPUT);
+  Serial.begin(9600);            // Initialize serial communications with the PC
+  SPI.begin();                   // Init SPI bus
+  lcd.begin(16, 2);              // Begin the LCD with 16 colums and 2 rows
+  mfrc522.PCD_Init();            // Init MFRC522 card
+  pinMode(servoPin, OUTPUT);     //define the Servo as an  output
+  pinMode(freeLED, OUTPUT);      //define the green LED as an output
+  pinMode(occupiedLED, OUTPUT);  //define the red LED as an output
+  pinMode(Buzzer, OUTPUT);       //define the Buzzer as an output
+  pinMode(doorButton, INPUT);    //define the button as an input
 
-  lockServo.write(90);
+  lockServo.write(90);  //start the servo in the 90º position, locked
 
-  digitalWrite(freeLED, HIGH);
-  lockServo.attach(servoPin);
-  lcd.print("pass the card");
+  digitalWrite(freeLED, HIGH);  //turn on the green LED
+  lockServo.attach(servoPin);   //attach a Servo to a pin
+  lcd.print("pass the card");   //write on the LCD to "pass the card"
 }
 
 void loop() {
   int stateButton = digitalRead(doorButton);
-  Serial.println(stateButton);
-  delay(500);
 
   // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
   MFRC522::MIFARE_Key key;
@@ -67,12 +57,12 @@ void loop() {
 
   //-------------------------------------------
 
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  // check if there are any new cards
   if (!mfrc522.PICC_IsNewCardPresent()) {
     return false;
   }
 
-  // Select one of the cards
+  // read the card if there is a card
   if (!mfrc522.PICC_ReadCardSerial()) {
     return false;
   }
@@ -82,17 +72,14 @@ void loop() {
 
 
 
-  if (verifyNameOfCard(key, block, len, status) && (stateButton == 1) && (lockState == true)) {
-    openDoor();
-  }
-  else if (verifyNameOfCard(key, block, len, status) && (stateButton == 1) && (lockState == false)) {
-    closeDoor();
-  }
-  else if (verifyNameOfCard(key, block, len, status) && (stateButton == 0) && (lockState == false)) {
-    doorNotClosed();
-  }
-  else {
-    cardDenied();
+  if (verifyNameOfCard(key, block, len, status) && (stateButton == 1) && (lockState == true)) {          //if the card is correct, the button is pressed and the Servo is in the locked possition
+    openDoor();                                                                                          //call the openDoor fucntion
+  } else if (verifyNameOfCard(key, block, len, status) && (stateButton == 1) && (lockState == false)) {  //if the card is correct, the button is pressed and the Servo is in the unlocked possition
+    closeDoor();                                                                                         //call the closeDoor fucntion
+  } else if (verifyNameOfCard(key, block, len, status) && (stateButton == 0) && (lockState == false)) {  //if the card is correct, the button is not pressed and the Servo is in the unlocked possition
+    doorNotClosed();                                                                                     //call the doorNotClosed fucntion
+  } else {                                                                                               //in every other case where none of those apply
+    cardDenied();                                                                                        //call the cardDenied fucntion
   }
 
   mfrc522.PICC_HaltA();
@@ -101,7 +88,7 @@ void loop() {
 
 
 
-bool verifyNameOfCard(MFRC522::MIFARE_Key key, byte block, byte len, MFRC522::StatusCode status) {
+bool verifyNameOfCard(MFRC522::MIFARE_Key key, byte block, byte len, MFRC522::StatusCode status) {  //function to verify the name of the card
   Serial.print(F("Name: "));
 
   byte buffer1[18];
@@ -137,58 +124,59 @@ bool verifyNameOfCard(MFRC522::MIFARE_Key key, byte block, byte len, MFRC522::St
 
   Serial.println(F("**End Reading**"));
 
-  if ((Nome == "Silva") || (Nome == "Master")) {
-    return true;
+  if ((Nome == "Silva") || (Nome == "Master")) {  //if the name of the card is the same as the name defiened
+    return true;                                  //return that it's correct
   } else {
-    return false;
+    return false;  //return that it's inncorrect
   }
 }
 
 void openDoor() {
-  lcd.clear();
-  digitalWrite(occupiedLED, LOW);
-  digitalWrite(freeLED, HIGH);
-  lcd.print("Door opening");
-  tone(Buzzer, 1000, 1000);
-  delay(1000);
-  lockServo.write(0);
-  lcd.clear();
-  lcd.print("pass the card");
-  lockState = false;
+  lcd.clear();                     //clear the LCD of the previous message
+  digitalWrite(occupiedLED, LOW);  //turn off the occupied LED
+  digitalWrite(freeLED, HIGH);     //turn on the free LED
+  lcd.print("Door opening");       //print on the LCD "door opening"
+  tone(Buzzer, 1000, 1000);        //buzzer makes a sound
+  delay(1000);                     //delay the code for 1000 milliseconds (1 second)
+  lockServo.write(0);              //turn the servo to the 0º position, unlocked
+  lcd.clear();                     //clear the LCD of the previous message
+  lcd.print("pass the card");      //print on the LCD "pass the card"
+  lockState = false;               //change the lockState to false, meaning the locker is now unlocked
 }
 
 void closeDoor() {
-  lcd.clear();
-  digitalWrite(occupiedLED, HIGH);
-  digitalWrite(freeLED, LOW);
-  lcd.print("Door closing");
-  tone(Buzzer, 1000, 1000);
-  delay(1000);
-  lockServo.write(90);
-  lockState = true;
-  lcd.clear();
-  lcd.print("pass the card");
+  lcd.clear();                      //clear the LCD of the previous message
+  digitalWrite(occupiedLED, HIGH);  //turn on the occupied LED
+  digitalWrite(freeLED, LOW);       //turn off the free LED
+  lcd.print("Door closing");        //write on the LCD "door closing"
+  tone(Buzzer, 1000, 1000);         //make the buzzer make a soound
+  delay(1000);                      //delay the code for 1000 milliseconds (1 second)
+  lockServo.write(90);              //turn the servo to the 90º position, locked
+  lockState = true;                 //change the lockState to false, meaning the locker is now locked
+  lcd.clear();                      //clear the LCD of the previous message
+  lcd.print("pass the card");       //write on the LCD "pass the card"
 }
 
 void doorNotClosed() {
-  lcd.clear();
-  lcd.print("Close the door");
-  tone(Buzzer, 500, 1000);
-  delay(1000);
-  tone(Buzzer, 500, 1000);
-  lcd.clear();
-  lcd.print("pass the card");
+  lcd.clear();                  //clear the LCD of the previous message
+  lcd.print("Close the door");  //write on the LCD "close the door"
+  tone(Buzzer, 500, 1000);      //make the buzzer make a soound
+  delay(1000);                  //delay the code for 1000 milliseconds (1 second)
+  tone(Buzzer, 500, 1000);      //make the buzzer make a soound
+  delay(1000);                  //delay the code for 1000 milliseconds (1 second)
+  lcd.clear();                  //clear the LCD of the previous message
+  lcd.print("pass the card");   //write on the LCD "pass the card"
 }
 
 void cardDenied() {
-  lcd.clear();
-  lcd.print("card was denied");
-  tone(Buzzer, 1000, 200);
-  delay(200);
-  tone(Buzzer, 1000, 200);
-  delay(200);
-  tone(Buzzer, 200, 200);
-  delay(500);
-  lcd.clear();
-  lcd.print("pass the card");
+  lcd.clear();                   //clear the LCD of the previous message
+  lcd.print("card was denied");  //write on the LCD "card was denied"
+  tone(Buzzer, 1000, 200);       //make the buzzer make a soound
+  delay(200);                    //delay the code for 200 milliseconds
+  tone(Buzzer, 1000, 200);       //make the buzzer make a soound
+  delay(200);                    //delay the code for 200 milliseconds
+  tone(Buzzer, 200, 200);        //make the buzzer make a soound
+  delay(200);                    //delay the code for 200 milliseconds
+  lcd.clear();                   //clear the LCD of the previous message
+  lcd.print("pass the card");    //write on the LCD "pass the card"
 }
